5d7570734dcb46c3a1cbd5a6fb0a78af6b1d562c
Antoine <velgheantoine8@gmail.com>
2026-06-16 11:47:11 +0200

    Portfolio finalisé avec style et interactivités

---
+++ script.js
@@ -0,0 +1,20 @@
+// Compteur de clics personnalisé
+let nbLikes = 0;
+const boutonLike = document.getElementById("mon-bouton-like");
+
+if (boutonLike) {
+    boutonLike.addEventListener("click", () =>{
+        nbLikes = nbLikes + 1;
+boutonLike.textContent = "❤️ J'aime (" + nbLikes + ")";
+});
+}
+// Compteur de clics personnalisé
+let nbdisLikes = 0;
+const boutondisLike = document.getElementById("mon-bouton-dislike");
+
+if (boutondisLike) {
+    boutondisLike.addEventListener("click", () =>{
+        nbdisLikes = nbdisLikes + 1;
+boutondisLike.textContent = "💔 Je n'aime pas (" + nbdisLikes + ")";
+});
+}
