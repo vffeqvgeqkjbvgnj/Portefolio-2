d7f858cc43b678e45f6df35ef76781a5f1f3ad9a
Antoine <velgheantoine8@gmail.com>
2026-06-25 12:18:52 +0200

    arc enc iel

--- Snake.js
+++ Snake.js
@@ -1,5 +1,5 @@
-// Variable pour savoir si le jeu a démarré
 let jeuLance = false;
+let score = 0; 
 
 function setup() {
   createCanvas(400, 400);
@@ -9,26 +9,26 @@ function setup() {
   snake.push([90, 110]);
   snake_direction = "right";
   
-  apples = []
-  create_apples()
+  apples = [];
+  create_apples();
+  score = 0;
 }
 
 function create_apples() {
   for (i = 0; i < 10; i++) {
     x = Math.floor(random(0, 40));
     y = Math.floor(random(0, 40));
-    apples.push([x * 10, y * 10])
+    apples.push([x * 10, y * 10]);
   }
 }
 
 function draw_apples() {
-  fill(255, 0, 0)
-  stroke(255, 0, 0)
+  textSize(10); 
+  textAlign(CENTER, CENTER); 
   apples.forEach((apple) => {
-    rect(apple[0],apple[1], 10, 10)
+    text("🍎", apple[0] + 5, apple[1] + 5); 
   });
 }
- 
 
 function keyPressed() {
   if (!jeuLance && (key === 's' || key === 'S')) {
@@ -67,47 +67,61 @@ function next_direction() {
 }
 
 function move_snake() {
-  check_collision()
-  head = snake[0]
-  eat = false
+  check_collision();
+  head = snake[0];
+  eat = false;
   apples.forEach((apple) => {
     if (head[0] == apple[0] && head[1] == apple[1]) {
-      pos = apples.indexOf(apple)
-      apples.splice(pos, 1)
-      eat = true
+      pos = apples.indexOf(apple);
+      apples.splice(pos, 1);
+      eat = true;
       
+      score += 10; 
+
       let new_x = Math.floor(random(0, 40));
       let new_y = Math.floor(random(0, 40));
       apples.push([new_x * 10, new_y * 10]);
     }
   });
   if (eat == false) {
-    snake.pop(); // supprime le dernier élément d'un tableau
+    snake.pop(); 
   }
-  snake.unshift(snake_next_direction);  //ajoute un élément au début d'un tableau
+  snake.unshift(snake_next_direction); 
 }
 
 function check_collision() {
-  head = snake[0]
+  head = snake[0];
   snake.forEach((element) => {
     if (head != element && head[0] == element[0] && head[1] == element[1]) {
-      noLoop()
+      noLoop();
     }
   });
 }
 
 function draw() {
-  background(220);
-  stroke(255, 255, 255);
+  background(20, 30, 50); 
+  
+  stroke(255, 255, 255, 30); 
   for (i=0; i < 40; i++)
   {
    line(i*10,0,i*10,400);
    line(0, i*10,400,i*10);
   }
-  stroke(0, 255, 0);
-  fill(0, 255, 0)
-  snake.forEach((element) => {
-    rect(element[0],element[1], 10, 10)
+  
+  snake.forEach((element, index) => {
+    colorMode(HSB, 255); 
+    let teinte = (index * 15) % 255; 
+    fill(teinte, 200, 255);
+    stroke(teinte, 255, 255);
+    rect(element[0], element[1], 10, 10);
+    colorMode(RGB, 255); 
   });
-  draw_apples()
+  
+  draw_apples();
+
+  fill(255);
+  noStroke();
+  textSize(20);
+  textAlign(LEFT, TOP); 
+  text("Score : " + score, 10, 10);
 }
