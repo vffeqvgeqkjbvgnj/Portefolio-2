b9fc502e2cf0cb455b691188fe9992e0031c18d5
Antoine <velgheantoine8@gmail.com>
2026-06-25 12:00:31 +0200

    Le sanke fonctionne mais le dit pas a Euge,ie

--- Snake.js
+++ Snake.js
@@ -1,38 +1,48 @@
-if (keypress == s)
-    function setup() {
+// Variable pour savoir si le jeu a démarré
+let jeuLance = false;
+
+function setup() {
   createCanvas(400, 400);
   snake = [];
   snake.push([90, 90]);
   snake.push([90, 100]);
   snake.push([90, 110]);
   snake_direction = "right";
-  setInterval(() => {
-    keyPressed();
-    next_direction();
-    move_snake();
-  }, 250);
+  
+  // Le setInterval a été retiré d'ici pour être mis dans keyPressed plus bas
+  
   apples = []
   create_apples()
- }
+}
 
- function create_apples() {
+function create_apples() {
   for (i = 0; i < 10; i++) {
     x = Math.floor(random(0, 40));
     y = Math.floor(random(0, 40));
     apples.push([x * 10, y * 10])
   }
- }
+}
 
- function draw_apples() {
+function draw_apples() {
   fill(255, 0, 0)
   stroke(255, 0, 0)
   apples.forEach((apple) => {
     rect(apple[0],apple[1], 10, 10)
   });
- }
+}
  
 
- function keyPressed() {
+function keyPressed() {
+  // SI LE JEU N'EST PAS LANCÉ ET QU'ON APPUIE SUR S, ON LANCE LE JEU
+  if (!jeuLance && (key === 's' || key === 'S')) {
+    jeuLance = true;
+    setInterval(() => {
+      keyPressed();
+      next_direction();
+      move_snake();
+    }, 250);
+  }
+
   if (keyCode == UP_ARROW && snake_direction != "down") {
     snake_direction = "up";
   } else if (keyCode == DOWN_ARROW && snake_direction != "up") {
@@ -42,9 +52,10 @@ if (keypress == s)
   } else if (keyCode == RIGHT_ARROW && snake_direction != "left") {
     snake_direction = "right";
   }
- }
+return false; 
+}
 
- function next_direction() {
+function next_direction() {
   snake_next_direction = null;
   if (snake_direction == "up") {
     snake_next_direction = [snake[0][0], snake[0][1] - 10];
@@ -55,8 +66,8 @@ if (keypress == s)
   } else if (snake_direction == "right") {
     snake_next_direction = [snake[0][0] + 10, snake[0][1]];
   }
- }
- function move_snake() {
+}
+function move_snake() {
   check_collision()
   head = snake[0]
   eat = false
@@ -71,18 +82,18 @@ if (keypress == s)
     snake.pop(); // supprime le dernier élément d'un tableau
   }
   snake.unshift(snake_next_direction);  //ajoute un élément au début d'un tableau
- }
+}
 
- function check_collision() {
+function check_collision() {
   head = snake[0]
   snake.forEach((element) => {
     if (head != element && head[0] == element[0] && head[1] == element[1]) {
       noLoop()
     }
   });
- }
+}
 
- function draw() {
+function draw() {
   background(220);
   stroke(255, 255, 255);
   for (i=0; i < 40; i++)
@@ -96,4 +107,4 @@ if (keypress == s)
     rect(element[0],element[1], 10, 10)
   });
   draw_apples()
- }
+}
